9e6c4492b79cfd37f8c808b25ada0ec4f5b87a66
serenademola <serenademola98@gmail.com>
2023-04-02 17:38:38 +0200

    New layout

--- assets/js/work.js
+++ assets/js/work.js
@@ -1,67 +1,52 @@
+// Play hide and seek with css class HEADER
 $(document).ready(function() {
-  var images = [
-      {src: "/images/ANNUARIO_1.jpg", alt: "Annuario_1", caption: "Annuario della Ricerca Iuav ✹ Editorial and Data Visualization ✹ 2022"},
-      {src: "/images/ANNUARIO_2.jpg", alt: "Annuario_2", caption: "Annuario della Ricerca Iuav ✹ Editorial and Data Visualization ✹ 2022"},
-      {src: "/images/ANNUARIO_3.jpg", alt: "Annuario_3", caption: "Annuario della Ricerca Iuav ✹ Editorial and Data Visualization ✹ 2022"},
-      {src: "/images/ANNUARIO_4.jpg", alt: "Annuario_4", caption: "Annuario della Ricerca Iuav ✹ Editorial and Data Visualization ✹ 2022"},
+  $('.text_container').find('.hidden-text').hide(); // nasconde il testo all'avvio della pagina
 
-      {src: "/images/APC_1.jpg", alt: "APC_1", caption: "Al Perpetuo Crepuscolo ✹ Seminar, Workshop and Identity ✹ 2022-2023"},
-      {src: "/images/APC_2.jpg", alt: "APC_2", caption: "Al Perpetuo Crepuscolo ✹ Seminar, Workshop and Identity ✹ 2022-2023"},
-      {src: "/images/APC_3.jpg", alt: "APC_3", caption: "Al Perpetuo Crepuscolo ✹ Seminar, Workshop and Identity ✹ 2022-2023"},
-      {src: "/images/APC_4.jpg", alt: "APC_4", caption: "Al Perpetuo Crepuscolo ✹ Seminar, Workshop and Identity ✹ 2022-2023"},
-
-
-      {src: "/images/Cronografie_1.jpg", alt: "Cronografie_1", caption: "Cronografie. Riflessioni sulla percezione e raffigurazione del tempo ✹ BA Thesis ✹ 2020"},
-      {src: "/images/Cronografie_2.jpg", alt: "Cronografie_2", caption: "Cronografie. Riflessioni sulla percezione e raffigurazione del tempo ✹ BA Thesis ✹ 2020"},
-      {src: "/images/Cronografie_3.jpg", alt: "Cronografie_3", caption: "Cronografie. Riflessioni sulla percezione e raffigurazione del tempo ✹ BA Thesis ✹ 2020"},
-      {src: "/images/Cronografie_4.jpg", alt: "Cronografie_4", caption: "Cronografie. Riflessioni sulla percezione e raffigurazione del tempo ✹ BA Thesis ✹ 2020"},
-
-      
-
-      {src: "/images/Atlante_1.jpg", alt: "Atlante_1", caption: "Atlante della trasformazione digitale della PA ✹ Editorial and Data Visualization ✹ 2022"},
-      {src: "/images/Atlante_2.jpg", alt: "Atlante_2", caption: "Atlante della trasformazione digitale della PA ✹ Editorial and Data Visualization ✹ 2022"},
-      {src: "/images/Atlante_3.jpg", alt: "Atlante_3", caption: "Atlante della trasformazione digitale della PA ✹ Editorial and Data Visualization ✹ 2022"},
-
-    ];
-    var currentImage = Math.floor(Math.random() * images.length);
-
-    $("img").attr("src", images[currentImage].src);
-    $("img").attr("alt", images[currentImage].alt);
-    $(".column.caption").text(images[currentImage].caption);
-
-    function changeImage(back) {
-      if (back) {
-        currentImage = (currentImage - 1 + images.length) % images.length;
-      } else {
-        currentImage = (currentImage + 1) % images.length;
-      }
-      $("img").fadeOut(0, function() {
-        $("img").attr("src", images[currentImage].src);
-        $("img").attr("alt", images[currentImage].alt);
-        $(".column.caption").text(images[currentImage].caption);
-        $("img").fadeIn(0);
-      });
-    }
-
-    setInterval(changeImage, 4000);
-
-    $("img").on("click", function(event) {
-    if (event.clientX < $(this).width() / 2) {
-      changeImage(true);
-    } else {
-      changeImage();
-    }
-    });
-
-  
-  
-  const cursor = document.querySelector('.cursor');
-  document.addEventListener('mousemove',(e) => {
-  cursor.style.left = e.clientX + 'px';
-  cursor.style.top = e.clientY + 'px';
-  }) 
-
-
+  $('.extra').click(function() {
+    var $text_container = $(this).closest('.text_container'); // trova il genitore .text_container del bottone
+    $text_container.find('.hidden-text').toggle(); // alterna la visibilità del testo all'interno del genitore .text_container
   });
+});
 
-  
+// Play hide and seek with css class SECTIONS 
+$(document).ready(function() {
+  $('.text_container-section').find('.hidden-text-section').css('visibility', 'hidden'); // nasconde il testo all'avvio della pagina
+
+  $('.extra-section').click(function() {
+    var $text_container = $(this).closest('.text_container-section'); // trova il genitore .text_container del bottone
+    var $hidden_text = $text_container.find('.hidden-text-section');
+    
+    if ($hidden_text.css('visibility') === 'hidden') {
+      $hidden_text.css('visibility', 'visible'); // mostra il testo se è invisibile
+    } else {
+      $hidden_text.css('visibility', 'hidden'); // altrimenti nascondilo
+    }
+  });
+});
+
+
+// Play hide and seek with css class section
+$(document).ready(function() {
+  $('.text_container-section').find('.hidden-text-section').hide(); // nasconde il testo all'avvio della pagina
+
+  $('.extra-section').click(function() {
+    var $text_container = $(this).closest('.text_container-section'); // trova il genitore .text_container del bottone
+    $text_container.find('.hidden-text-section').toggle(); // alterna la visibilità del testo all'interno del genitore .text_container
+  });
+});
+
+// When the user scrolls down 30px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
