3e30740b7ffdbf1157e25bc2ad5ebedb1cc5377f
serenademola <serenademola98@gmail.com>
2023-05-08 11:29:52 +0200

    Options

--- assets/my-script.js
+++ assets/my-script.js
@@ -1,9 +1,4 @@
-document.addEventListener('mousemove', function(e){
-    var cursor = document.querySelector('.circle-cursor');
-    cursor.style.top = (e.pageY - 10) + 'px';
-    cursor.style.left = (e.pageX - 10) + 'px';
-  });
-  
+ 
 
   var images = [
     "url(/images/bg_1.png)",
