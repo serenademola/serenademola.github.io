20f784d32cd6c38fc242dcccbbf0433299749e45
serenademola <serenademola98@gmail.com>
2023-05-15 19:34:32 +0200

    New column

--- assets/my-script.js
+++ assets/my-script.js
@@ -4,11 +4,16 @@ $(document).ready(function() {
     {src: "/images/Pagine queer/pagine-queer_1.jpg", alt: "Pagine queer"},
     {src: "/images/Pagine queer/pagine-queer_2.jpg", alt: "Pagine queer"},
     {src: "/images/Pagine queer/pagine-queer_3.jpg", alt: "Pagine queer"},
+    {src: "/images/Pagine queer/pagine-queer_4.jpg", alt: "Pagine queer"},
     {src: "/images/Pagine queer/pagine-queer_5.jpg", alt: "Pagine queer"},
     {src: "/images/Pagine queer/pagine-queer_6.jpg", alt: "Pagine queer"},
     {src: "/images/Pagine queer/pagine-queer_7.jpg", alt: "Pagine queer"},
     {src: "/images/APC/APC_3.jpg", alt: "APC"},
     {src: "/images/APC/APC_2.jpg", alt: "APC"},
+    {src: "/images/APC/APC_1.jpg", alt: "APC"},
+    {src: "/images/APC/APC_4.jpg", alt: "APC"},
+    {src: "/images/APC/APC_5.jpg", alt: "APC"},
+
   ];
   var currentImage = Math.floor(Math.random() * images.length);
 
@@ -28,7 +33,7 @@ $(document).ready(function() {
     });
   }
 
-  setInterval(changeImage, 2000);
+  setInterval(changeImage, 4000);
 
   $(".showreel-image").on("click", function(event) {
     if (event.clientX < $(this).width() / 2) {
