7b6d4a89d83546aa2880978468a2518aa48c82f4
serenademola <serenademola98@gmail.com>
2025-05-06 18:55:23 +0200

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
 var owl = $('.owl-carousel');
 owl.owlCarousel({
     loop:true,
-    nav:true,
-    margin:10,
+    nav:false,
+    margin:20,
     responsive:{
         0:{
             items:1
@@ -10,14 +10,12 @@ owl.owlCarousel({
         600:{
             items:2
         },            
-        960:{
+        1000:{
             items:3
-        },
-        1200:{
-            items:4
         }
     }
 });
+
 owl.on('mousewheel', '.owl-stage', function (e) {
     if (e.deltaY>0) {
         owl.trigger('next.owl');
