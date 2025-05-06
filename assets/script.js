85b92bf80cb56120594e9cf856b6b4cf8b2464d9
serenademola <serenademola98@gmail.com>
2025-05-06 18:43:17 +0200

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -12,40 +12,15 @@ $(document).ready(function () {
         }
     });
     
-    // Abilita la funzionalità mousewheel utilizzando il plugin jquery.mousewheel.js
-    //$('.owl-carousel').on('mousewheel', '.owl-stage', function (e) {
-    //    if (scrolling) return;  // Se stiamo già scorrendo, non fare nulla
-    //    scrolling = true;
-    //
-   //     if (e.originalEvent.deltaY > 0) {
-    //        owl.trigger('next.owl.carousel');  // Scorri avanti
-    //    } else {
-    //        owl.trigger('prev.owl.carousel');  // Scorri indietro
-    //    }
+// Gestisci il click del mouse sugli elementi .owl-item
+owl.on('click', '.owl-item .image', function (e) {
+    var viewportWidth = $(window).width();
+    var clickX = e.clientX;
 
-    // Disabilita il drag quando l'utente sta selezionando il testo
-    $(document).on('selectstart', function () {
-        isTextSelecting = true;
-        owl.trigger('destroy.owl.carousel');  // Disabilita temporaneamente il carosello
-    });
-
-    // Riabilita il drag una volta che la selezione del testo è terminata
-    $(document).on('mouseup', function () {
-        if (isTextSelecting) {
-            owl.owlCarousel();  // Riabilita il carosello dopo il rilascio della selezione
-            isTextSelecting = false;
-        }
-    });
-
-    // Gestisci il click del mouse sugli elementi .owl-item
-    owl.on('click', '.owl-item .image', function (e) {
-        var viewportWidth = $(window).width();
-        var clickX = e.clientX;
-
-        if (clickX < viewportWidth / 2) {
-            owl.trigger('prev.owl.carousel');
-        } else {
-            owl.trigger('next.owl.carousel');
-        }
-    });
+    if (clickX < viewportWidth / 2) {
+        owl.trigger('prev.owl.carousel');
+    } else {
+        owl.trigger('next.owl.carousel');
+    }
+});
 });
