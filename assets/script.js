fcf597a7addd943bcb0616d60e73da759216ac8e
serenademola <serenademola98@gmail.com>
2025-05-06 19:07:15 +0200

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,37 @@
-var owl = $('.owl-carousel');
-owl.owlCarousel({
-    loop:true,
-    nav:false,
-    margin:20,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:2
-        },            
-        1000:{
-            items:3
+$(document).ready(function () {
+    var owl = $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 20,
+        autoWidth: true,
+        mouseDrag: false,
+        nav: false,
+        responsive: {
+            0: { items: 1 },
+            600: { items: 2 },
+            1000: { items: 3 }
         }
-    }
-});
+    });
+    
+    // Abilita la funzionalità mousewheel utilizzando il plugin jquery.mousewheel.js
+    //$('.owl-carousel').on('mousewheel', '.owl-stage', function (e) {
+    //    if (scrolling) return;  // Se stiamo già scorrendo, non fare nulla
+    //    scrolling = true;
+    //
+   //     if (e.originalEvent.deltaY > 0) {
+    //        owl.trigger('next.owl.carousel');  // Scorri avanti
+    //    } else {
+    //        owl.trigger('prev.owl.carousel');  // Scorri indietro
+    //    }
 
-// Gestisci rotellina del mouse sugli elementi .owl-item
-owl.on('mousewheel', '.owl-stage', function (e) {
-    if (e.deltaY>0) {
-        owl.trigger('next.owl');
-    } else {
-        owl.trigger('prev.owl');
-    }
-    e.preventDefault();
-});
+    // Gestisci il click del mouse sugli elementi .owl-item
+    owl.on('click', '.owl-item .image', function (e) {
+        var viewportWidth = $(window).width();
+        var clickX = e.clientX;
 
-// Gestisci il click del mouse sugli elementi .owl-item
-owl.on('click', '.owl-item .image', function (e) {
-    var viewportWidth = $(window).width();
-    var clickX = e.clientX;
-
-    if (clickX < viewportWidth / 2) {
-        owl.trigger('prev.owl');
-    } else {
-        owl.trigger('next.owl');
-    }
+        if (clickX < viewportWidth / 2) {
+            owl.trigger('prev.owl.carousel');
+        } else {
+            owl.trigger('next.owl.carousel');
+        }
+    });
 });
